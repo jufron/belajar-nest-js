dde8f355c7e5c24f446a487d7fce33a2c5c6ef3c
jufron <jufrontamoama@gmail.com>
2026-08-07 11:32:30 +0800

    belajar membuat controller dengan penambahkan module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user/user.controller';
+import { AvatarController } from './user/avatar/avatar.controller';
+import { ProfileController } from './user/profile/profile.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController, AvatarController, ProfileController],
   providers: [AppService],
 })
 export class AppModule {}
